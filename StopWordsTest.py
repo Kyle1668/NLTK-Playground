009f4676ddc911b500c388e3b60eafb11c2dd929
Kyle1668 <kyledevinobrien1@gmail.com>
2017-12-24 17:26:03 -0800

    Added stop words test

---
+++ StopWordsTest.py
@@ -0,0 +1,21 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+from texts import Texts
+
+gettysburg_address = word_tokenize(Texts.gettysburg_address)
+stop_words = set(stopwords.words("English"))
+sanitized_gettysburg_address = []
+
+for word in gettysburg_address:
+    if word not in stop_words:
+        sanitized_gettysburg_address.append(word)
+
+print(sanitized_gettysburg_address)
+
+# ['Four', 'score', 'seven', 'years', 'ago', 'fathers', 'brought', 'forth', 'continent', ','
+# , 'new', 'nation', ',', 'conceived', 'Liberty', ',', 'dedicated', 'proposition', 'men', '
+# created', 'equal', '.', 'Now', 'engaged', 'great', 'civil', 'war', ',', 'testing',
+# 'whether', 'nation', ',', 'nation', 'conceived', 'dedicated', ',', 'long',
+# 'endure', '.', 'We', 'met', 'great', 'battle-field', 'war', '.', 'We',
+# 'come', 'dedicate', 'portion', 'field', ',', 'final', 'resting', 'place'] ...
